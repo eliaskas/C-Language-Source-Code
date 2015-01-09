6c0e82eb80b5f9c9b2e530341f1a446861bc5080
eliaskas <eliaskas@gmail.com>
2015-01-09 11:27:50 +0200

    HelloWorld
    ...

---
+++ HelloWorld/hw.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    
+    printf("Hello world!\n\n\n");
+
+    system("PAUSE");
+
+    return EXIT_SUCCESS;
+}
+
+
---
+++ HelloWorld/hw_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef HW_PRIVATE_H
+#define HW_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"20.10.30.-34"
+#define VER_MAJOR	20
+#define VER_MINOR	10
+#define VER_RELEASE	30
+#define VER_BUILD	-34
+#define COMPANY_NAME	"HTA-THETA Labs"
+#define FILE_VERSION	"20.10.30.-40"
+#define FILE_DESCRIPTION	"The famous Hello World"
+#define INTERNAL_NAME	"hw.c"
+#define LEGAL_COPYRIGHT	"No copyright at all!"
+#define LEGAL_TRADEMARKS	"Trademarks belong to their companies."
+#define ORIGINAL_FILENAME	"hw.c"
+#define PRODUCT_NAME	"hw.exe"
+#define PRODUCT_VERSION	"20.10.30.-40"
+
+#endif /*HW_PRIVATE_H*/
